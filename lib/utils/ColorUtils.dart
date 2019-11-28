a40efbafa094b541b7f7080298c62d513948f314
David Caceres <david.a.caceres@gmail.com>
2019-11-27 23:21:49 -0300

    jamboree-client-1 Color de fondo de "paragraph_conf" solo funciona en el primer párrafo

--- lib/utils/ColorUtils.dart
+++ lib/utils/ColorUtils.dart
@@ -3,13 +3,17 @@ import 'package:flutter/material.dart';
 ///Retorna el color rgb, en caso de no poder realizar la
 ///conversion obtiene el color del tema para el fondo
 ///y entrega este por defecto
-Color getBackgroundColor(BuildContext context, List<int> rgb) {
+Color getBackgroundColor(BuildContext context, List<int> rgb,
+    [Color bgDefaultColor]) {
   Color color;
   try {
     color = Color.fromRGBO(rgb[0], rgb[1], rgb[2], 1);
   } catch (ex) {
     print('Error al convertir color ${rgb.toString()}');
-    color = Colors.white;
+    if (bgDefaultColor == null)
+      color = Colors.white;
+    else
+      color = bgDefaultColor;
   }
   return color;
 }
@@ -17,13 +21,17 @@ Color getBackgroundColor(BuildContext context, List<int> rgb) {
 ///Retorna el color rgb, en caso de no poder realizar la
 ///conversion obtiene el color del para el texto tema y
 ///entrega este por defecto
-Color getTextColor(BuildContext context, List<int> rgb) {
+Color getTextColor(BuildContext context, List<int> rgb, [Color defaultColor]) {
   Color color;
   try {
     color = Color.fromRGBO(rgb[0], rgb[1], rgb[2], 1);
   } catch (ex) {
     print('Error al convertir color ${rgb.toString()}');
-    color = Colors.black87;
+    if (defaultColor == null) {
+      color = Colors.black87;
+    } else {
+      color = defaultColor;
+    }
   }
   return color;
 }
